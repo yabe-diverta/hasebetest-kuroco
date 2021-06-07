29fd01d4f93c07870a44291b57498c40068cc6c3
HisashiHasebe <hhasebe@diverta.co.jp>
2021-06-07 17:55:40 +0800

    表示文言修正

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">hasebeテスト用のサイトです。</h1>
+      <h1 class="title">hasebeテストサイト。</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
