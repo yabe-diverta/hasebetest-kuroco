b4693493099845c9200e8bbaf86a3fa139ea02ef
HisashiHasebe <hhasebe@diverta.co.jp>
2021-06-07 21:46:50 +0800

    blog amend

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,35 +1,3 @@
-import Link from "next/link";
-
- export async function getStaticProps() {
-  const res = await fetch(`${f283061574fa6a086add6556a2cf3c06}/rcms-api/1/blog`);
-  const data = await res.json();
-
-  return {
-   props: {
-     data,
-   },
-   revalidate: 60, 
-  };
- }
-
- export default function Blog(props) {
-  const { data } = props;
-  const lists = data.list;
-  console.log(lists);
-
- return (
-  <div>
-   <ul className="p-4">
-    {lists.map((list, index) => (
-     <Link href={`/blog/${list.slug}`} key={index}>
-      <a>
-       <li className="m-2 text-blue-500">
-        {list.subject}
-       </li>
-      </a>
-     </Link>
-    ))}
-   </ul>
-</div>
-);
-}
+<template>
+    <h1>ブログ</h1>
+</template>
