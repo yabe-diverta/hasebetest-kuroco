7f5f0e9633613b4072b539dea1dfc6fb8b627387
HisashiHasebe <hhasebe@diverta.co.jp>
2021-06-08 10:10:42 +0800

    news修正

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -10,7 +10,7 @@
 export default {
     async asyncData ({ $axios }) {
         try {
-            const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/1/newsdetail')
+            const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/1/news')
             return { response }
         }catch (e) {
             console.log(e.message)
