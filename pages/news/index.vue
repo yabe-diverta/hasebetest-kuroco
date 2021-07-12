2903f069c906c000dbf9a7267c50969e810d3ca3
HisashiHasebe <hhasebe@diverta.co.jp>
2021-07-12 15:42:24 +0800

    プレビュー機能対応

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -8,10 +8,26 @@
 
 <script>
 export default {
-    async asyncData ({ $axios }) {
+    async asyncData ({ route, $axios }) {
+
+        const requestNews = async () => {
+            const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/2/news');
+            return { response };
+        };
+        const requestNewsPreview = async (previewToken) => {
+            const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/2/news/preview' + '?preview_token=' + previewToken);
+            return { response: { list: [response.details] } };
+        };
+
+        // URLのクエリに、
+        // preview_tokenが存在する場合は、previewエンドポイントへ、
+        // preview_tokenが存在しない場合は、newsエンドポイントへ、
+        // リクエストします。
         try {
-            const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/1/news')
-            return { response }
+            const previewToken = route.query.preview_token;
+            return previewToken !== undefined ?
+                await requestNewsPreview(previewToken) :
+                await requestNews();
         }catch (e) {
             console.log(e.message)
         }
