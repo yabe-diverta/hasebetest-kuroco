58434ddc0d4992cd63d62334ccc4d358a086d2d4
HisashiHasebe <hhasebe@diverta.co.jp>
2021-11-30 13:29:45 +0800

    bootstrapの読み込み

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'stylesheet', integrity: 'sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css', crossorigin: 'anonymous'}],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
