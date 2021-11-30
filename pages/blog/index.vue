86a80533cae7eb83b22f759422f68f543dc0ea33
HisashiHasebe <hhasebe@diverta.co.jp>
2021-11-30 15:47:33 +0800

    ブログページのデザインをカードに修正

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div><p>ブログ一覧ページ</p>
-    <div class="container">
-	    <div class="row">
-        <div v-for="n in response.list" :key="n.slug" class="col-4">
-          <img :src="n.ext_col_01.url" width = 120>
+    <div class="row">
+      <div v-for="n in response.list" :key="n.slug" class="col-sm-4" style="width: 18rem;">
+        <div class="card">
+        <img :src="n.ext_col_01.url" width = 120>
+          <div class="container-fluid">
+          <h5 class="card-title">{{n.subject}}</h5>
+          <p class="card-text" v-html="n.contents"></p>
+          <nuxt-link :to="'/blog/'+ n.slug" class="btn btn-primary">ブログを見る</nuxt-link><br/>
           <nuxt-link :to="'/blog/'+ n.slug">{{n.ymd}} {{n.subject}}</nuxt-link>
+          </div>
         </div>
       </div>
     </div>
