59b20d5eaee6854c49175bbc1cb5e94562713a95
HisashiHasebe <hhasebe@diverta.co.jp>
2021-11-30 16:45:54 +0800

    ナビバー修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,23 @@
     <header class="p-header" style="background-color:#696969;">
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
       <div class="container-fluid">
-       <a class="navbar-brand" href="#">Navbar</a>
+       <a class="navbar-brand" href="#">テストサイト</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-             <a class="nav-link active" aria-current="page" href="#">Home</a>
+             <a class="nav-link active" aria-current="page" href="/">Home</a>
            </li>
            <li class="nav-item">
-             <a class="nav-link" href="#">Link</a>
+             <a class="nav-link active" aria-current="page" href="/blog/">ブログ</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link active" aria-current="page" href="/news/">ニュース</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link active" aria-current="page" href="/form/">フォーム</a>
            </li>
            <li class="nav-item dropdown">
              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -25,9 +31,6 @@
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item" href="#">Something else here</a></li>
               </ul>
-            </li>
-            <li class="nav-item">
-             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
          </ul>
          <form class="d-flex">
