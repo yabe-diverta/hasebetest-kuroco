b8fafefc16d6825d847d137e014e8f0040b8b928
HisashiHasebe <hhasebe@diverta.co.jp>
2021-12-15 14:04:35 +0800

    jquery使ってないので削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,11 +28,6 @@ module.exports = {
           'sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p',
         crossorigin: 'anonymous',
       },
-      {
-        src: 'https://code.jquery.com/jquery-3.6.0.min.js',
-        integrity: 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=',
-        crossorigin: 'anonymous',
-      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     link: [
