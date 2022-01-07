3f886a7ded9d24ad5aa7af7defdba02b1fd22395
HisashiHasebe <hhasebe@diverta.co.jp>
2022-01-07 11:42:39 +0800

    newsページの多言語化

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ module.exports = {
     [
       'nuxt-i18n',
       {
-        strategy: 'prefix',
+        strategy: 'prefix_and_default',
         // 切り替える言語を定義
         locales: [
           { code: 'ja', file: 'ja.json' },
