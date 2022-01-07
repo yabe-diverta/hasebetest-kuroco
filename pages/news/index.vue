41799946d2a320779773e7baf45c7470806f6c11
HisashiHasebe <hhasebe@diverta.co.jp>
2022-01-07 14:16:48 +0800

    newsの多言語対応

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,20 +1,36 @@
 <template>
   <div>
     <div v-for="n in response.list" :key="n.slug">
-      <nuxt-link :to="'/news/'+ n.slug">{{n.ymd}} {{n.subject}}</nuxt-link>
+      <nuxt-link :to="localePath('/news/'+ n.slug)">{{n.ymd}} {{n.subject}}</nuxt-link>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    async asyncData ({ $axios }) {
-        try {
-            const response = await $axios.$get(process.env.BASE_URL + '/rcms-api/1/news')
-            return { response }
-        }catch (e) {
-            console.log(e.message)
-        }
+  async asyncData({ $axios, app }) {
+    if(app.i18n.locale === 'ja'){
+      try {
+       const response = await $axios.$get(
+         process.env.BASE_URL + '/rcms-api/1/news'
+       )
+       console.log(response)
+       return { response }
+      } catch (e) {
+       console.log(e.message)
+       }
     }
+    else{
+      try {
+       const response = await $axios.$get(
+         process.env.BASE_URL + '/rcms-api/1/news?_lang=en'
+       )
+       console.log(response)
+       return { response }
+      } catch (e) {
+        console.log(e.message)
+       }
+    }
+  },
 }
 </script>
