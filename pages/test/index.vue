e309ce2941e42fb81843d8e687d17f151753b516
HisashiHasebe <hhasebe@diverta.co.jp>
2022-01-10 16:34:11 +0800

    更新テスト2

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-      <p>KurocoFilesキャッシュのテスト2</p>
+      <p>KurocoFilesキャッシュのテスト3</p>
       <p><a :href="response.details.ext_col_01.url">APIのレスポンスのテストpdf</a></p>
       <p><a href="https://hasebetest.g.kuroco-img.app/v=1234567890/files/user/testfile.pdf">KurocFilesを直接参照テストpdf</a></p>
       <p>APIのレスポンスのテスト画像<img :src="response.details.ext_col_02.url"/></p>
-      <p>KurocFilesを直接参照テスト画像<img src="https://hasebetest.g.kuroco-img.app/v=1234567891/files/user/gazo.png"/></p>
+      <p>KurocFilesを直接参照テスト画像<img src="https://hasebetest.g.kuroco-img.app/v=1234567892/files/user/gazo.png"/></p>
     </div>
 </template>
 
