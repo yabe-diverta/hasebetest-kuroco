e654681e7dbf7e9c41dc7a9b077c5a0b5e0d6d47
HisashiHasebe <hhasebe@diverta.co.jp>
2022-01-21 15:56:08 +0800

    businessページ追加

---
+++ pages/business/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img :src="response.details.ext_col_01.url">
+    <div>{{response.details.ext_col_02}}</div>
+    <div>{{response.details.ext_col_03}}</div>
+
+    <div v-for="n in response.details.ext_col_04" :key="n.slag" >
+      <img :src="n.ext_col_04.url" />
+      <div>{{ n.ext_col_03 }}</div>
+      <div>{{ n.ext_col_05 }}</div>
+      <div>{{ n.ext_col_06 }}</div>
+    </div>
+  </div>
+</template>
+
+
+
+<style>
+</style>
+
+<script>
+export default {
+  async asyncData({ $axios, app }) {
+    try {
+      const response = await $axios.$get(
+        process.env.BASE_URL + '/rcms-api/16/business/1023'
+        )
+        console.log(response)
+        return { response }
+        } catch (e) {
+          console.log(e.message)
+          }
+  },
+}
+</script>
