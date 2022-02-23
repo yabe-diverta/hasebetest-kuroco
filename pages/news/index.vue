ef9fb349ed18325fddfd1d5371db9e36004171c0
HisashiHasebe <hhasebe@diverta.co.jp>
2022-02-23 17:18:16 +0800

    パラメータ変更時は更新する形で一旦実装

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <p>ニュース一覧ページ</p>
-
+    <p>現在のページ {{response.pageInfo.pageNo}}/{{response.pageInfo.totalPageCnt}}</p>
     <div v-for="n in response.list" :key="n.slug">
       <nuxt-link :to="'/news/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link>
     </div>
+
+    <ul style="list-style: none; display: flex">
+      <li>前へ</li>
+      <li v-for="i in response.pageInfo.totalPageCnt" :key="i">        
+        <nuxt-link :to="{query:{pageID:i}}" replace>{{i}}</nuxt-link>
+      </li>
+      <li>次へ</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+//  beforeRouteUpdate(to, from, next) {
+//    next()
+//    window.location.reload()
+//  },
+  watch: {
+    '$route.query'() {
+      this.$router.go(0)
+    }
+  },
   async asyncData({ $axios, query }) {
     try {
       const response = await $axios.$get(
