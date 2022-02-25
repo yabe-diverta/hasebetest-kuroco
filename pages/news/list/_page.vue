322cf7b1d648caa1aca2015804dab9463c06b860
HisashiHasebe <hhasebe@diverta.co.jp>
2022-02-25 16:51:31 +0800

    記事のリストをクエリパラメータではなく、ディレクトリで分けるように修正

--- pages/news/list/_page.vue
+++ pages/news/list/_page.vue
@@ -1,40 +1,33 @@
 <template>
   <div>
-    <p>ニュース一覧ページ</p>
+    <p>ニュース一覧ページ{{ this.$route.params.page }}</p>
     <p>現在のページ {{response.pageInfo.pageNo}}/{{response.pageInfo.totalPageCnt}}</p>
     <div v-for="n in response.list" :key="n.slug">
-      <nuxt-link :to="'/news/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link>
+      <nuxt-link :to="'/news/detail/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link>
     </div>
 
     <ul style="list-style: none; display: flex">
-      <li>前へ</li>
+      <li v-if="response.pageInfo.pageNo === 1">前へ</li>
+      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo -1)">前へ</nuxt-link></li>
       <li v-for="i in response.pageInfo.totalPageCnt" :key="i">        
-        <nuxt-link :to="{query:{pageID:i}}" replace>{{i}}</nuxt-link>
+        <nuxt-link :to="'/news/list/' + i">{{i}}</nuxt-link>
       </li>
-      <li>次へ</li>
+      <li v-if="response.pageInfo.pageNo === response.pageInfo.totalPageCnt">次へ</li>
+      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo +1)">次へ</nuxt-link></li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-//  beforeRouteUpdate(to, from, next) {
-//    next()
-//    window.location.reload()
-//  },
-  watch: {
-    '$route.query'() {
-      this.$router.go(0)
-    }
-  },
-  async asyncData({ $axios, query }) {
+  async asyncData({ $axios, params}) {
     try {
       const response = await $axios.$get(
         process.env.BASE_URL + '/rcms-api/1/news',
         {
           params: {
             cnt: 4,
-            pageID: query.pageID,
+            pageID: params.page,
           },
         }
       )
