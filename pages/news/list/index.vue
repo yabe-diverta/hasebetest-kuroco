dc178a1fc1c674a03fc8766d7e30d992bd02e63f
HisashiHasebe <hhasebe@diverta.co.jp>
2022-03-08 12:55:40 +0800

    fix:news/list/index.vue

--- pages/news/list/index.vue
+++ pages/news/list/index.vue
@@ -2,19 +2,30 @@
   <div>
     <p>ニュース一覧ページ{{ this.$route.params.page }}</p>
     <div v-for="n in response.list" :key="n.slug">
-      <nuxt-link :to="'/news/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link >
+      <nuxt-link :to="'/news/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link>
     </div>
+
+    <ul style="list-style: none; display: flex">
+      <li v-if="response.pageInfo.pageNo === 1">前へ</li>
+      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo -1)">前へ</nuxt-link></li>
+      <li v-for="i in response.pageInfo.totalPageCnt" :key="i">        
+        <nuxt-link :to="'/news/list/' + i">{{i}}</nuxt-link>
+      </li>
+      <li v-if="response.pageInfo.pageNo === response.pageInfo.totalPageCnt">次へ</li>
+      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo +1)">次へ</nuxt-link></li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ $axios, params }) {
+  async asyncData({ $axios, params}) {
     try {
       const response = await $axios.$get(
-        process.env.BASE_URL + '/rcms-api/1/news',{
+        process.env.BASE_URL + '/rcms-api/1/news',
+        {
           params: {
-            pageID: params.page
+            pageID: params.page,
           },
         }
       )
