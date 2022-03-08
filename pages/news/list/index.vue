09a50257faec2cda4a8526b164f4b8a5f498f060
HisashiHasebe <hhasebe@diverta.co.jp>
2022-03-08 11:49:03 +0800

    /news/list/index.vueを追加してみる

---
+++ pages/news/list/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>ニュース一覧ページ{{ this.$route.params.page }}</p>
+    <div v-for="n in response.list" :key="n.slug">
+      <nuxt-link :to="'/news/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    try {
+      const response = await $axios.$get(
+        process.env.BASE_URL + '/rcms-api/1/news',{
+          params: {
+            pageID: params.page
+          },
+        }
+      )
+      return { response }
+    } catch (e) {
+      console.log(e.message)
+    }
+  },
+}
+</script>
