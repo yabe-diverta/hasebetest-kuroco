600aed7f231a761e080a2d83ff459c677ac744d8
HisashiHasebe <hhasebe@diverta.co.jp>
2022-03-29 11:26:26 +0800

    fix:登録できるようになった。変数名はあとで調整

--- pages/subscribe/index.vue
+++ pages/subscribe/index.vue
@@ -2,22 +2,38 @@
   <div>
     <form>
     <h1>マガジン登録</h1>
-    <input name="email" type="text">
-    <button v-on:click="SubscribeSubmit">購読する</button>
+      <p v-if="loginStatus !== null">
+        {{ emailEntered }}{{ resultMessage }}
+      </p>
+    <input v-model="emailEntered" name="email" type="email" placeholder="email">
+    <button v-on:click.prevent="SubscribeSubmit">購読する</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      emailEntered: '',
+      loginStatus: null,
+      resultMessage: null
+      }
+  },
   methods: {
     async SubscribeSubmit(e) {
       try {
+        const payload = {
+                    email: this.emailEntered
+                }
         // post data
-        const { id } = await this.$axios.$post(
-          process.env.BASE_URL + `/rcms-api/13/magazine_subscribe/1`,{"email":"test7@example.com"})
+        const response = await this.$axios.$post(
+          process.env.BASE_URL + `/rcms-api/13/magazine_subscribe/1`, payload)
+          this.loginStatus = 'success'
+          this.resultMessage = 'の配信購読者登録をしました。'
       } catch (e) {
-         console.log(e)
+         this.loginStatus = 'failure'
+         this.resultMessage = 'の配信購読者登録ができませんでした。'
       }
     },
   },
