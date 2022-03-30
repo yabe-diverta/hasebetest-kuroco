faa74dc433b706c0e8a5acdf98e269001a4830ee
HisashiHasebe <hhasebe@diverta.co.jp>
2022-03-30 15:56:21 +0800

    テスト送信の機能も実装

--- pages/subscribe/index.vue
+++ pages/subscribe/index.vue
@@ -2,13 +2,13 @@
   <div>
     <form>
     <h1>マガジン登録</h1>
-      <p v-if="loginStatus !== null">
+      <p v-if="resultMessage !== null">
         {{ resultMessage }}
       </p>
     <input v-model="emailEntered" name="email" type="email" placeholder="email">
     <button v-on:click.prevent="subscribeSubmit">購読する</button>
     <button v-on:click.prevent="unsubscribeSubmit">購読解除する</button>
-    <button>テスト送信する</button>
+    <button v-on:click.prevent="sendTestMail">テスト送信する</button>
     </form>
   </div>
 </template>
@@ -18,7 +18,6 @@ export default {
   data () {
     return {
       emailEntered: '',
-      loginStatus: null,
       resultMessage: null,
       }
   },
@@ -32,10 +31,8 @@ export default {
         // post data
         const response = await this.$axios.$post(
           process.env.BASE_URL + `/rcms-api/13/magazine_subscribe/1`, payload)
-          this.loginStatus = 'success'
           this.resultMessage = response.messages[0]
       } catch (error) {
-         this.loginStatus = 'failure'
          this.resultMessage = error.response.data.errors[0].message
       }
     },
@@ -48,13 +45,25 @@ export default {
         // post data
         const response = await this.$axios.$post(
           process.env.BASE_URL + `/rcms-api/13/magazine_unsubscribe/1`, payload)
-          this.loginStatus = 'success'
           this.resultMessage = response.messages[0]
       } catch (error) {
-         this.loginStatus = 'failure'
          this.resultMessage = error.response.data.errors[0].message
       }
     },
-  },
+    //テスト送信するクリック時の動作
+    async sendTestMail() {
+      try {
+        const payload = {
+                    mail_to: this.emailEntered
+                }
+        // post data
+        const response = await this.$axios.$post(
+          process.env.BASE_URL + `/rcms-api/13/sendMail`, payload)
+          this.resultMessage = 'テストメールを送信しました。'
+      } catch (error) {
+         console.log(error.message)
+      }
+    }
+  }
 }
 </script>
