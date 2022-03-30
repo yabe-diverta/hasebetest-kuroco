ed9303997e38c8f0e5be090d3db29f994ef10876
HisashiHasebe <hhasebe@diverta.co.jp>
2022-03-30 14:10:21 +0800

    購読する購読解除するの機能を追加

--- pages/subscribe/index.vue
+++ pages/subscribe/index.vue
@@ -3,10 +3,12 @@
     <form>
     <h1>マガジン登録</h1>
       <p v-if="loginStatus !== null">
-        {{ emailEntered }}{{ resultMessage }}
+        {{ resultMessage }}
       </p>
     <input v-model="emailEntered" name="email" type="email" placeholder="email">
-    <button v-on:click.prevent="SubscribeSubmit">購読する</button>
+    <button v-on:click.prevent="subscribeSubmit">購読する</button>
+    <button v-on:click.prevent="unsubscribeSubmit">購読解除する</button>
+    <button>テスト送信する</button>
     </form>
   </div>
 </template>
@@ -17,11 +19,12 @@ export default {
     return {
       emailEntered: '',
       loginStatus: null,
-      resultMessage: null
+      resultMessage: null,
       }
   },
   methods: {
-    async SubscribeSubmit(e) {
+    //購読するクリック時の動作
+    async subscribeSubmit() {
       try {
         const payload = {
                     email: this.emailEntered
@@ -30,10 +33,26 @@ export default {
         const response = await this.$axios.$post(
           process.env.BASE_URL + `/rcms-api/13/magazine_subscribe/1`, payload)
           this.loginStatus = 'success'
-          this.resultMessage = 'の配信購読者登録をしました。'
-      } catch (e) {
+          this.resultMessage = response.messages[0]
+      } catch (error) {
          this.loginStatus = 'failure'
-         this.resultMessage = 'の配信購読者登録ができませんでした。'
+         this.resultMessage = error.response.data.errors[0].message
+      }
+    },
+    //購読するクリック時の動作
+    async unsubscribeSubmit() {
+      try {
+        const payload = {
+                    email: this.emailEntered
+                }
+        // post data
+        const response = await this.$axios.$post(
+          process.env.BASE_URL + `/rcms-api/13/magazine_unsubscribe/1`, payload)
+          this.loginStatus = 'success'
+          this.resultMessage = response.messages[0]
+      } catch (error) {
+         this.loginStatus = 'failure'
+         this.resultMessage = error.response.data.errors[0].message
       }
     },
   },
