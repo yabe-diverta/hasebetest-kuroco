6adf5d46de18c1fd529bee196903f232a759d52c
HisashiHasebe <hhasebe@diverta.co.jp>
2022-04-29 10:41:14 +0800

    url末尾に/追加

--- pages/news/list/_page.vue
+++ pages/news/list/_page.vue
@@ -2,17 +2,17 @@
   <div>
     <p>ニュース一覧ページ{{ this.$route.params.page }}</p>
     <div v-for="n in response.list" :key="n.slug">
-      <nuxt-link :to="'/news/' + n.slug">{{ n.ymd }} {{ n.subject }}</nuxt-link>
+      <nuxt-link :to="'/news/' + n.slug +'/'">{{ n.ymd }} {{ n.subject }}</nuxt-link>
     </div>
 
     <ul style="list-style: none; display: flex">
       <li v-if="response.pageInfo.pageNo === 1">前へ</li>
-      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo -1)">前へ</nuxt-link></li>
+      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo -1) +'/'">前へ</nuxt-link></li>
       <li v-for="i in response.pageInfo.totalPageCnt" :key="i">        
-        <nuxt-link :to="'/news/list/' + i">{{i}}</nuxt-link>
+        <nuxt-link :to="'/news/list/' + i +`/`">{{i}}</nuxt-link>
       </li>
       <li v-if="response.pageInfo.pageNo === response.pageInfo.totalPageCnt">次へ</li>
-      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo +1)">次へ</nuxt-link></li>
+      <li v-else><nuxt-link :to="'/news/list/' + (response.pageInfo.pageNo +1) +'/'">次へ</nuxt-link></li>
     </ul>
   </div>
 </template>
