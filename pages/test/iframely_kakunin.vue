8d435090942c6d9cfb6176bf28db56ae2794785f
HisashiHasebe <hhasebe@diverta.co.jp>
2022-07-25 11:52:17 +0800

    update: iframelyの確認ページデプロイ

---
+++ pages/test/iframely_kakunin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="title">{{ response.details.subject }}</h1>
+    <div class="post" v-html="response.details.ext_col_01"></div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        {
+          async: true,
+          charset: "utf-8",
+          src: "https://iframely.kuroco-front.app/embed.js?cancel=0",
+        }
+      ],
+    };
+  },
+  async asyncData({ $axios, params }) {
+    return {
+      response: await $axios.$get(`https://sample-support-kuroco.a.kuroco.app/rcms-api/26/test/253`),
+    };
+  },
+};
+</script>
+
