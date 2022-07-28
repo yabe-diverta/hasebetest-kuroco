cb3a37fd810538c513e6be904f9c089e2117cb26
HisashiHasebe <hhasebe@diverta.co.jp>
2022-07-28 14:37:49 +0800

    update:news_list 追加

---
+++ pages/news_list/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="title">{{ response.details.subject }}</h1>
+    <div class="post" v-html="response.details.contents"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    return {
+      response: await $axios.$get(`/rcms-api/7/newsdetail/${params.slug}`),
+    };
+  },
+};
+</script>
---
+++ pages/news_list/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>ニュース一覧ページ{{ this.$route.query.page }}</p>
+    <div v-for="n in response.list" :key="n.slug">
+      <nuxt-link :to="`/${n.topics_id}`">
+        {{ n.ymd }} {{ n.subject }}
+      </nuxt-link>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, route }) {
+    return {
+      response: await $axios.$get('/rcms-api/7/news', {
+        params: {
+          pageID: route.query.page || 1,
+        },
+      }),
+    };
+  },
+};
+</script>
