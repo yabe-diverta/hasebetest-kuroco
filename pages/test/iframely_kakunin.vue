ce02a200d35dae56bc0af82cee881c52be592903
YuseiAbe <yabe@diverta.co.jp>
2022-08-16 08:10:18 +0900

    fix: iframely call

--- pages/test/iframely_kakunin.vue
+++ pages/test/iframely_kakunin.vue
@@ -1,13 +1,8 @@
 <template>
   <div v-if="response">
     <h1 class="title">{{ response.details.subject }}</h1>
+    <!-- eslint-disable-next-line vue/no-v-html -->
     <div class="post" v-html="response.details.ext_col_01"></div>
-
-    <div class="iframely-embed">
-      <div class="iframely-responsive">
-        <a data-iframely-url href="https://twitter.com/MYEmbassyTokyo"></a>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -23,45 +18,24 @@ export default {
   data() {
     return {
       response: null,
-      iframesCount: null,
     }
   },
-  watch: {
-    iframesCount: {
-      immediate: true,
-      handler(count) {
-        if (count === 0) {
-          this.loadIframely()
-        }
-      },
-    },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      const iframes = Array.from(document.querySelectorAll('.post iframe'))
-      // sets iframe elements count, does count-down to detect all iframes loaded.
-      this.iframesCount = iframes.length
-      iframes.forEach(
-        (iframe) =>
-          (iframe.onload = () => (this.iframesCount = this.iframesCount - 1))
-      )
-    })
-  },
-  methods: {
-    // adds iframely scripts manually, because it won't load external elements before loading iframes done.
-    loadIframely() {
-      const elm = document.createElement('script')
-      const iframelyResource = {
-        async: true,
-        charset: 'utf-8',
-        src: 'https://iframely.kuroco-front.app/embed.js?cancel=0',
-        // src: '//cdn.iframe.ly/embed.js',
-      }
-      Object.entries(iframelyResource).forEach(([k, v]) =>
-        elm.setAttribute(k, v)
-      )
-      document.body.appendChild(elm)
-    },
+  head() {
+    return {
+      script: [
+        {
+          async: true,
+          charset: 'utf-8',
+          src: 'https://iframely.kuroco-front.app/embed.js?cancel=0',
+        },
+      ],
+    }
   },
 }
 </script>
+
+<style>
+.post figure.media {
+  display: block;
+}
+</style>
